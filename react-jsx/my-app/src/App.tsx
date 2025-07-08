7ad790d3ca8b1403c063959c1ea1eac01881ec28
Duncan Harrison <duncanharrison3@gmail.com>
2025-07-08 05:19:02 +0000

    Completed react-jsx exercise.

---
+++ react-jsx/my-app/src/App.tsx
@@ -0,0 +1,37 @@
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '../../../../../vite.svg';
+import './App.css';
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>React JSX</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          Total Button Clicks: {count}
+        </button>
+        <p>
+          JSX is a JavaScript extension that allows developers to write code for
+          dynamic DOM creation in a HTML-adjacent syntax.
+        </p>
+      </div>
+      <p className="read-the-docs">
+        React uses JSX to construct the DOM so it can display on a browser page,
+        thereby simplifying the development process.
+      </p>
+    </>
+  );
+}
+
+export default App;
---
+++ react-jsx/react-jsx-quiz-notes.md
@@ -0,0 +1,38 @@
+# react-jsx-quiz-notes
+
+## Quiz Questions
+
+Answer the following questions in the provided markdown file before turning in this exercise:
+
+- What is JSX?
+  JSX is a JavaScript extension that allows developers to write code for dynamic DOM creation in a HTML-adjacent syntax.
+- How does React use JSX to render components?
+  React uses JSX to construct the DOM so it can display on a browser page, thereby simplifying the development process.
+
+## Notes
+
+All student notes should be written here.
+
+How to write `Code Examples` in markdown
+
+for JS:
+
+```javascript
+const data = 'Howdy';
+```
+
+for HTML:
+
+```html
+<div>
+  <p>This is text content</p>
+</div>
+```
+
+for CSS:
+
+```css
+div {
+  width: 100%;
+}
+```
